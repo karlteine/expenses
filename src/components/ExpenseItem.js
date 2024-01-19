b3cbc24a8da848bc0d5d8ad7cae8b0c281abaddb
karlteine <karlkivisild@gmail.com>
2024-01-19 14:11:43 +0200

    Added objects to components

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,13 +1,14 @@
 import './ExpenseItem.css'
 
-function ExpenseItem(){
+function ExpenseItem(props){
+    console.log(props)
+
     return (
         <div className="expense-item">
-            <div>Date</div>
-            <div className="expense-item__description">
-                <h2>Title</h2>
-                <div className="expense-item__price">
-                    Amount</div>
+            <div>{props.expenseData.date.toString()}</div>
+            <div>
+                <h2>{props.expenseData.title}</h2>
+                <div>{props.expenseData.price}</div>
             </div>
         </div>
     )
