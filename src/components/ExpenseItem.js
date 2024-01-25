9121cc0981564627ab74b80952ca1798061b4dc2
karlteine <karlkivisild@gmail.com>
2024-01-25 08:49:49 +0200

    Added Card design component

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,27 +1,21 @@
-import './ExpenseItem.css'
-import ExpenseDate from './ExpenseDate'
+import React from 'react';
+import ExpenseDate from './ExpenseDate';
 
-function ExpenseItem(props){
+function Card(props) {
+  const classes = 'card ' + props.className;
+  return <div className={classes}>{props.children}</div>;
+}
 
-    //const day = props.expenseData.date.toLocaleString
-        //('en-US', {day: '2-digit'})
-    //const month = props.expenseData.date.
-        //toLocaleString('en-US', {month: 'long'})
-    //const year = props.expenseData.date.getFullYear()
+function ExpenseItem(props) {
+  return (
+    <Card className="expense-item">
+      <ExpenseDate date={props.expenseData.date} />
+      <div className="expense-item__description">
+        <h2>{props.expenseData.title}</h2>
+        <div className="expense-item__price">{props.expenseData.price}</div>
+      </div>
+    </Card>
+  );
+}
 
-    return (
-        <div className="expense-item">
-            <ExpenseDate
-                date={props.expenseData.date}
-            /> 
-            <div className="expense-item__description
-                ">
-                <h2>{props.expenseData.title}</h2>
-                <div className="expense-item__price">
-                {props.expenseData.price}</div>
-            </div>
-        </div>
-    )
-} 
-
-export default ExpenseItem
+export default ExpenseItem;
--- src/components/Expenses.js
+++ src/components/Expenses.js
@@ -3,14 +3,13 @@ import './Expenses.css';
 import ExpenseItem from './ExpenseItem';
 
 function Expenses(props) {
-
     return (
-        <div className="expenses">
-            {props.expenses.map((expense) => (
-                <ExpenseItem key={expense.id} expenseData={expense} />
-            ))}
-        </div>
+      <div className="card expenses">
+        {props.expenses.map((expense, index) => (
+          <ExpenseItem key={index} expenseData={expense} />
+        ))}
+      </div>
     );
-}
+  }
 
 export default Expenses;
