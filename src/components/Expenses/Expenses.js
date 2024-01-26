aff1a9167e794bb2e828b591012863e669bbf253
karlteine <karlkivisild@gmail.com>
2024-01-26 12:47:28 +0200

    Display only expenses incurred in a specific year

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,15 +1,32 @@
-import Card from '../UI/Card'
+import React, { useState } from 'react';
+import Card from '../UI/Card';
 import './Expenses.css';
 import ExpenseItem from './ExpenseItem';
+import ExpensesFilter from './ExpensesFilter';
 
 const Expenses = (props) => {
-    
+    const [filteredYear, setFilteredYear] = useState('2023');
+
+    const filterChangeHandler = selectedYear => {
+        setFilteredYear(selectedYear);
+    };
+
+    const filteredExpenses = props.expenses.filter(expense => {
+        return expense.date.getFullYear().toString() === filteredYear;
+    });
+
     return (
         <Card className="expenses">
-                <ExpenseItem expenseData={props.expenses[0]} />
-                <ExpenseItem expenseData={props.expenses[1]} />
+            <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
+            {filteredExpenses.length === 0 ? (
+                <p className="no-year">No expenses found for the selected year.</p>
+            ) : (
+                filteredExpenses.map((expense, index) => (
+                    <ExpenseItem key={index} expenseData={expense} />
+                ))
+            )}
         </Card>
-    )
-}
+    );
+};
 
 export default Expenses;
